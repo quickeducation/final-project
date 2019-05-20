df03a7887e7f01286aa52c1e7a43e32d0c5b10de
Zachary Palmer <zachbhs@uw.edu>
2019-05-20 15:49:41 -0700

    Cleaned up scripts

--- public/fifteen.js
+++ public/fifteen.js
@@ -14,9 +14,6 @@
   function $(id){
       return document.getElementById(id);
   }
-  /**
-   * Handles the sign in button press.
-   */
   function toggleSignIn() {
     if (firebase.auth().currentUser) {
       // [START signout]
@@ -33,13 +30,10 @@
         alert('Please enter a password.');
         return;
       }
-      // Sign in with email and pass.
-      // [START authwithemail]
       firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
-        // [START_EXCLUDE]
         if (errorCode === 'auth/wrong-password') {
           alert('Wrong password.');
         } else {
@@ -47,15 +41,11 @@
         }
         console.log(error);
         document.getElementById('quickstart-sign-in').disabled = false;
-        // [END_EXCLUDE]
       });
-      // [END authwithemail]
     }
     //document.getElementById('quickstart-sign-in').disabled = true;
   }
-  /**
-   * Handles the sign up button press.
-   */
+
   function handleSignUp() {
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
@@ -67,45 +57,31 @@
       alert('Please enter a password.');
       return;
     }
-    // Sign in with email and pass.
-    // [START createwithemail]
     firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
       // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
-      // [START_EXCLUDE]
       if (errorCode == 'auth/weak-password') {
         alert('The password is too weak.');
       } else {
         alert(errorMessage);
       }
       console.log(error);
-      // [END_EXCLUDE]
     });
     firebase.database().ref('users/' + firebase.auth().currentUser).set({
       email: email,
       score : 0
     });
-    // [END createwithemail]
   }
-  /**
-   * Writes the user's data to the database.
-   */
-  // [START basic_write]
+
   function writeUserData(userId, email) {
     firebase.database().ref('users/' + userId).set({
       email: email,
       score : 0
     });
   }
-  // [END basic_write]
 
-  /**
-   * Saves a new post to the Firebase DB.
-   */
-  // [START write_fan_out]
   function writeNewSet(uid, email, title) {
-    // A post entry.
     var setData = {
       author: email,
       uid: uid,
@@ -113,48 +89,37 @@
       title: title
     };
 
-    // Get a key for a new Post.
+    // Get a key for a new set.
     var newSetKey = firebase.database().ref().child('sets').push().key;
 
-    // Write the new post's data simultaneously in the posts list and the user's post list.
+    // Write the new sets's data simultaneously in the posts list and the user's post list.
     var updates = {};
     updates['/sets/' + newSetKey] = setData;
     updates['/user-sets/' + uid + '/' + newSetKey] = setData;
 
     return firebase.database().ref().update(updates);
   }
-  // [END write_fan_out]
-  /**
-   * Creates a new post for the current user.
-   */
+
   function newSetForCurrentUser(title) {
-    // [START single_value_read]
     var userId = firebase.auth().currentUser.uid;
     return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
       var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
-      // [START_EXCLUDE]
       return writeNewSet(firebase.auth().currentUser.uid, firebase.auth().currentUser.email,
         title);
-      // [END_EXCLUDE]
     });
-    // [END single_value_read]
   }
 
   function newQuestionAnswer(setTitle, question, answer) {
-    // [START single_value_read]
     var userId = firebase.auth().currentUser.uid;
     return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
       var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
-      // [START_EXCLUDE]
       return writeQA(firebase.auth().currentUser.uid, setTitle,
         question, answer);
-      // [END_EXCLUDE]
     });
-    // [END single_value_read]
   }
 
   function writeQA(uid, setTitle, question, answer) {
-    // Get a key for a new Post.
+    // Get all sets.
     var ref = firebase.database().ref().child('sets');
 
     // This gets every value in every set
@@ -168,13 +133,6 @@
         }
        })
     });
-
-    // This gets all set IDs
-    /*ref.once("value").then(function(questionsSnapshot) {
-        for (var key in questionsSnapshot.val()) {
-              console.log(key);
-          }
-       });*/
   }
 
 
@@ -202,23 +160,16 @@
         var isAnonymous = user.isAnonymous;
         var uid = user.uid;
         var providerData = user.providerData;
-        // [START_EXCLUDE]
         $('quickstart-sign-in-status').textContent = 'Signed in';
         $('quickstart-sign-in').textContent = 'Sign out';
         writeUserData(user.uid, user.email);
-        //startDatabaseQueries();
-        // [END_EXCLUDE]
       } else {
         // User is signed out.
-        // [START_EXCLUDE]
         currentUID = null;
         $('quickstart-sign-in-status').textContent = 'Signed out';
         $('quickstart-sign-in').textContent = 'Sign in';
-        // [END_EXCLUDE]
       }
-    // [START_EXCLUDE silent]
     $('quickstart-sign-in').disabled = false;
-    // [END_EXCLUDE]
   }
   window.onload = function() {
     $('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
@@ -229,11 +180,9 @@
 
     $('set-form').onsubmit = function(e) {
       e.preventDefault();
-      //var text = $('new-post-question').value;
       var title = $('new-post-title').value;
       if (title) {
         newSetForCurrentUser(title).then(function() {
-          //myPostsMenuButton.click();
         });
         //$('new-post-question').value = '';
         //$('new-post-title').value = '';
@@ -242,14 +191,13 @@
 
     $('qa-form').onsubmit = function(e) {
       e.preventDefault();
-      //var text = $('new-post-question').value;
       var question = $('new-question').value;
+      // Key is serialized to fit in a key, remove the + when pulling info
       question = question.split(' ').join('+');
       var answer = $('new-answer').value;
       var title = $('new-post-title').value;
       if (question && answer && title) {
         newQuestionAnswer(title, question, answer).then(function() {
-          //myPostsMenuButton.click();
         });
         $('new-question').value;
         $('new-answer').value;
