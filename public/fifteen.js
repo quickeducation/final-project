9309b6c042851457e736fbd412c1699652b5ac76
Zachary Palmer <zachbhs@uw.edu>
2019-05-20 15:26:27 -0700

    Added ability to add question-answer pairs for sets

--- public/fifteen.js
+++ public/fifteen.js
@@ -104,12 +104,12 @@
    * Saves a new post to the Firebase DB.
    */
   // [START write_fan_out]
-  function writeNewSet(uid, email, title, body) {
+  function writeNewSet(uid, email, title) {
     // A post entry.
     var setData = {
       author: email,
       uid: uid,
-      body: body,
+      body: '',
       title: title
     };
 
@@ -127,18 +127,57 @@
   /**
    * Creates a new post for the current user.
    */
-  function newSetForCurrentUser(title, text) {
+  function newSetForCurrentUser(title) {
     // [START single_value_read]
     var userId = firebase.auth().currentUser.uid;
     return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
       var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
       // [START_EXCLUDE]
       return writeNewSet(firebase.auth().currentUser.uid, firebase.auth().currentUser.email,
-        title, text);
+        title);
       // [END_EXCLUDE]
     });
     // [END single_value_read]
   }
+
+  function newQuestionAnswer(setTitle, question, answer) {
+    // [START single_value_read]
+    var userId = firebase.auth().currentUser.uid;
+    return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
+      var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+      // [START_EXCLUDE]
+      return writeQA(firebase.auth().currentUser.uid, setTitle,
+        question, answer);
+      // [END_EXCLUDE]
+    });
+    // [END single_value_read]
+  }
+
+  function writeQA(uid, setTitle, question, answer) {
+    // Get a key for a new Post.
+    var ref = firebase.database().ref().child('sets');
+
+    // This gets every value in every set
+    ref.once("value").then(function(questionsSnapshot) {
+      return questionsSnapshot.forEach(function(questionSnapshot) {
+        if (questionSnapshot.val().title == setTitle && questionSnapshot.val().uid == uid) {
+          var updates = {};
+          updates['/sets/' + String(questionSnapshot.key) + '/body/' + question] = answer;
+          updates['/user-sets/' + uid + '/' + String(questionSnapshot.key) + '/body/' + question] = answer;
+          return firebase.database().ref().update(updates);
+        }
+       })
+    });
+
+    // This gets all set IDs
+    /*ref.once("value").then(function(questionsSnapshot) {
+        for (var key in questionsSnapshot.val()) {
+              console.log(key);
+          }
+       });*/
+  }
+
+
   /**
    * The ID of the currently signed-in User. We keep track of this to detect Auth state change events that are just
    * programmatic token refresh but not a User status change.
@@ -166,7 +205,6 @@
         // [START_EXCLUDE]
         $('quickstart-sign-in-status').textContent = 'Signed in';
         $('quickstart-sign-in').textContent = 'Sign out';
-        $('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
         writeUserData(user.uid, user.email);
         //startDatabaseQueries();
         // [END_EXCLUDE]
@@ -176,7 +214,6 @@
         currentUID = null;
         $('quickstart-sign-in-status').textContent = 'Signed out';
         $('quickstart-sign-in').textContent = 'Sign in';
-        $('quickstart-account-details').textContent = 'null';
         // [END_EXCLUDE]
       }
     // [START_EXCLUDE silent]
@@ -190,16 +227,32 @@
     // Listen for auth state changes
     firebase.auth().onAuthStateChanged(onAuthStateChanged);
 
-    $('message-form').onsubmit = function(e) {
+    $('set-form').onsubmit = function(e) {
       e.preventDefault();
-      var text = $('new-post-message').value;
+      //var text = $('new-post-question').value;
       var title = $('new-post-title').value;
-      if (text && title) {
-        newSetForCurrentUser(title, text).then(function() {
+      if (title) {
+        newSetForCurrentUser(title).then(function() {
           //myPostsMenuButton.click();
         });
-        $('new-post-message').value = '';
-        $('new-post-title').value = '';
+        //$('new-post-question').value = '';
+        //$('new-post-title').value = '';
+      }
+    };
+
+    $('qa-form').onsubmit = function(e) {
+      e.preventDefault();
+      //var text = $('new-post-question').value;
+      var question = $('new-question').value;
+      question = question.split(' ').join('+');
+      var answer = $('new-answer').value;
+      var title = $('new-post-title').value;
+      if (question && answer && title) {
+        newQuestionAnswer(title, question, answer).then(function() {
+          //myPostsMenuButton.click();
+        });
+        $('new-question').value;
+        $('new-answer').value;
       }
     };
   };
